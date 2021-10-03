ca88510f8697787d4594611e0c659805f03c31c5
AuroraKarow <karowmisaki@gmail.com>
2021-10-03 17:36:49 +0800

    Update directory

--- main.cpp
+++ main.cpp
@@ -7,13 +7,11 @@ using namespace mtx;
 int main(int argc, char *argv[], char *envp[])
 {
     cout << "hello, world." << endl;
-    matrix test = {{1, 2},
-                   {3, 4}};
-    cout << test << endl;
-    test.pos_idx(1) = 0;
+    matrix test = {{ 1, 2},
+                   { 3, 4}};
     cout << test << endl;
     string root_dir = ".../TensorLab/";
-    bitmap map(root_dir + "Src/Alice.jpg");
+    bitmap map(root_dir + "Src/Alice.png");
     map.save_img(root_dir + "Src", "Alice", BMIO_BMP, '/');
     return EXIT_SUCCESS;
 }
