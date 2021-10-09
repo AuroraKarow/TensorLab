c9f10e4c7d4b19738f3d5fea82cc0294aa9c58b3
AuroraKarow <karowmisaki@gmail.com>
2021-10-09 21:19:39 +0800

    Update neunet -> FCN normal layer

--- neunet.hpp
+++ neunet.hpp
@@ -1,13 +1,19 @@
 NEUNET_BEGIN
 
-struct Layer{};
+struct Layer
+{
+    uint64_t iActFuncType = SIGMOID;
+    virtual void setActFunc(uint64_t iActFuncIdx) { iActFuncType = iActFuncIdx;}
+    Layer() {}
+    Layer(uint64_t iActFuncIdx) : iActFuncType(iActFuncIdx) {}
+};
 
 class LayerFCN : public Layer
 {
 private:
     vect vecWeight;
 public:
-    LayerFCN() {}
+    LayerFCN(uint64_t iActFuncIdx = SIGMOID) : Layer(iActFuncIdx) {}
     bool SetInputOutputLnCnt(uint64_t iInputLnCnt, uint64_t iOutputLnCnt, double dRandBoundryFirst = 0.0, double dRandBoundrySecond = 0.0, double dAcc = 1e-05)
     {
         if(iInputLnCnt && iOutputLnCnt)
@@ -17,10 +23,16 @@ public:
         }
         else return false;
     }
-    LayerFCN(uint64_t iInputLnCnt, uint64_t iOutputLnCnt, double dRandBoundryFirst = 0.0, double dRandBoundrySecond = 0.0, double dAcc = 1e-05) {SetInputOutputLnCnt(iInputLnCnt, iOutputLnCnt, dRandBoundryFirst, dRandBoundrySecond, dAcc);}
+    LayerFCN(uint64_t iActFuncIdx, uint64_t iInputLnCnt, uint64_t iOutputLnCnt, double dRandBoundryFirst = 0.0, double dRandBoundrySecond = 0.0, double dAcc = 1e-05) : Layer(iActFuncIdx) {SetInputOutputLnCnt(iInputLnCnt, iOutputLnCnt, dRandBoundryFirst, dRandBoundrySecond, dAcc);}
+    set<vect> ForwProp(set<vect> &setInput) {return fc::Output(setInput, vecWeight);}
+    set<vect> BackProp(set<vect> &setInput, set<vect> &setGrad, double dLearnRate)
+    {
+        auto setGradBack = fc::GradLossToInput(setGrad, vecWeight);
+        vecWeight -= dLearnRate * fc::GradLossToWeight(setGrad, setInput);
+        return setGradBack;
+    }
 };
 
-class LayerActivate
-{};
+
 
 NEUNET_END
